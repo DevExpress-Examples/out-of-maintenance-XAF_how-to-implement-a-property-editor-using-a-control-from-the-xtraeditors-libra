8eb2a1da5c624da230841df59300daa94227e643
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/E232.Module.Win/WinModule.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace E232.Module.Win
+   <ToolboxItemFilter("Xaf.Platform.Win")> _
+   Public NotInheritable Partial Class E232WindowsFormsModule
+       Inherits ModuleBase
+
+      Public Sub New()
+         InitializeComponent()
+      End Sub
+   End Class
+End Namespace
