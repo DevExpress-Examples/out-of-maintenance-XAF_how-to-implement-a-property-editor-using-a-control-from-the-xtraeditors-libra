5a7e6f4723b3c03ea079d64f75185c923d3539af
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToSetMaskForCustomWinPropertyEdtior.Module.Win/WinModule.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace HowToSetMaskForCustomWinPropertyEdtior.Module.Win {
+   [ToolboxItemFilter("Xaf.Platform.Win")]
+   public sealed partial class HowToSetMaskForCustomWinPropertyEdtiorWindowsFormsModule : ModuleBase {
+      public HowToSetMaskForCustomWinPropertyEdtiorWindowsFormsModule() {
+         InitializeComponent();
+      }
+   }
+}
